8090c40dc67c409d38e0f19f4e58ba49e760117d
Дмитрий Быков <dimkson@yahoo.com>
2020-04-24 10:35:01 +0300

    Добавлена структура Complex.

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -10,12 +10,48 @@ namespace Lesson3
         {
             Menu.delMenu[] delMenu = new Menu.delMenu[] { Task01 };
             Menu menu = new Menu(delMenu);
+            menu.ChooseMenu();
         }
         #region Задание1
         static void Task01()
         {
-
+            Complex complex1;
+            complex1.im = 1;
+            complex1.re = 1;
+            Complex complex2;
+            complex2.im = 2;
+            complex2.re = 2;
+            Complex result = complex1.Plus(complex2);
+            Console.WriteLine(result.ToString());
+            result = complex1.Multi(complex2);
+            Console.WriteLine(result.ToString());
+            FC.Pause();
         }
+        struct Complex
+        {
+            public double im;
+            public double re;
+
+            public Complex Plus(Complex x)
+            {
+                Complex y;
+                y.im = im + x.im;
+                y.re = re + x.re;
+                return y;
+            }
+            public Complex Multi(Complex x)
+            {
+                Complex y;
+                y.im = im * x.im + re * x.im;
+                y.re = re * x.im + im * x.re;
+                return y;
+            }
+            public override string ToString()
+            {
+                return re + "+" + im + "i";
+            }
+        }
+
         #endregion
     }
 }
